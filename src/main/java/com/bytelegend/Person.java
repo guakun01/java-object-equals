38387724022c2124211b9eb36ea650dc9e58c2e4
pipiloveslife <Pain_Zzz@163.com>
2022-10-19 07:32:26 +0000

    Update at 2022-10-19T07:32:26.221468Z

--- src/main/java/com/bytelegend/Person.java
+++ src/main/java/com/bytelegend/Person.java
@@ -8,4 +8,21 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        if (obj instanceof Person) {
+            Person another = (Person) obj;
+            return this.firstName.equals(another.firstName) && this.lastName.equals(another.lastName);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.firstName) + Objects.hashCode(this.lastName);
+    }
 }
