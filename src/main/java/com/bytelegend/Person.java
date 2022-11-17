74fb94bd2f034fdc0aa511a192e53879657dd221
pamaomaoa <544060246@qq.com>
2022-11-17 16:01:14 +0000

    Update at 2022-11-17T16:01:14.452769Z

--- src/main/java/com/bytelegend/Person.java
+++ src/main/java/com/bytelegend/Person.java
@@ -8,4 +8,11 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    public boolean equals(Person person1, Person person2)
+    {
+        if (null == person1 || null == person2)
+            return false;
+        return person1.firstName.equals(person2.firstName) && person1.lastName.equals(person2.lastName);
+    }
 }
