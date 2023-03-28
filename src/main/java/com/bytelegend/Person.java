d2f855464a417f780877f2b4527fbdcce710f334
guakun01 <guakun01@gmail.com>
2023-03-28 22:44:48 +0800

    Update at Tue Mar 28 2023 22:44:54 GMT+0800 (中国标准时间)

--- src/main/java/com/bytelegend/Person.java
+++ src/main/java/com/bytelegend/Person.java
@@ -8,4 +8,14 @@ public class Person {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
+    @Override
+    public boolean equals(Object otherPerson) {
+        if (!(otherPerson instanceof Person)) {
+            return false;
+        }
+
+        Person other = (Person) otherPerson;
+        return other.firstName.equals(firstName) && other.lastName.equals(lastName);
+    }
 }
